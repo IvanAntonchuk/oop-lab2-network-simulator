235f7bd9bf017712b4beaba8660def9a159aedd3
Ivan Antonchuk <mmeisonan@gmail.com>
2026-05-24 15:28:34 +0300

    Setup project structure, CMake configuration, and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    QWidget w;
+    w.resize(800, 600);
+    w.show();
+    return a.exec();
+}
